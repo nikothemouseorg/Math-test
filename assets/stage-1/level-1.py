c2da37959144dd45536de5542471cd64dd8a3040
YaIiya <76162743+YaIiya@users.noreply.github.com>
2021-01-30 20:19:06 +1000

    Add files via upload

--- assets/stage-1/level-1.py
+++ assets/stage-1/level-1.py
@@ -4,13 +4,77 @@ is_completed = false
 a = choice(numbers)
 b = choice(numbers)
 answer = a + b
-myAnsw = int(input())
-def checkAnsw():
-    if myAnsw != answer:
-        print('Wrong')
-    elif myAnsw == answer:
-    print('Right')
-
+myAnsw = 0
+bonus = 100
 
 print('Question 1:')
 print(f'{a} + {b} = ???')
+myAnsw = int(input())
+if myAnsw != answer:
+    print('Wrong')
+    answerredCorrect = false
+    bonus -= 20
+elif myAnsw == answer:
+    print('Right')
+    answerredCorrect = true
+answerredCorrect1 = answerredCorrect
+
+
+
+a = choice(numbers)
+b = choice(numbers)
+answer = a + b
+print('Question 3:')
+print(f'{a} + {b} = ???')
+myAnsw = int(input())
+if myAnsw != answer:
+    print('Wrong')
+    answerredCorrect = false
+    bonus -= 20
+elif myAnsw == answer:
+    print('Right')
+    answerredCorrect = true
+answerredCorrect2 = answerredCorrect
+
+
+
+a = choice(numbers)
+b = choice(numbers)
+answer = a + b
+print('Question 4:')
+print(f'{a} + {b} = ???')
+myAnsw = int(input())
+if myAnsw != answer:
+    print('Wrong')
+    answerredCorrect = false
+    bonus -= 20
+elif myAnsw == answer:
+    print('Right')
+    answerredCorrect = true
+answerredCorrect2 = answerredCorrect
+
+
+
+a = choice(numbers)
+b = choice(numbers)
+answer = a + b
+print('Question 5:')
+print(f'{a} + {b} = ???')
+myAnsw = int(input())
+if myAnsw != answer:
+    print('Wrong')
+    answerredCorrect = false
+    bonus -= 20
+elif myAnsw == answer:
+    print('Right')
+    answerredCorrect = true
+answerredCorrect2 = answerredCorrect
+print(f'You earned points: 400 + {bonus} = {400 + bonus} points.')
+points.
+sleep(1)
+print('You has moved to next level!')
+lastStage1.write('1')
+lvlStage1.write('2')
+
+
+
